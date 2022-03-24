b2bced81cc568f46e05fa7f282b85f31b8c0cd05
Julie Bauné <91194921+julie-reine15@users.noreply.github.com>
2022-03-24 20:01:29 +0100

    Update translitteration_cy-lt.py

--- translitteration_cy-lt.py
+++ translitteration_cy-lt.py
@@ -1,10 +1,8 @@
 from collections import defaultdict
+from nltk import ngrams
 
 
-class Translitteration():
-
-    # def __init__(self, translit=None):
-    #     self.translit = translit
+class Translitteration:
 
     def rule_cy_lt(self):
 
@@ -14,44 +12,110 @@ class Translitteration():
         for line in file:
             for rule in line.split("\n"):
                 if len(rule.split("\t")) > 1:
-                    rules[rule.split("\t")[0]] = rule.split("\t")[1].strip()
+                    rules[rule.split("\t")[0]] = rule.split("\t")[1].strip("\n")
         return rules
 
-    def translit_cy_lt(self, input_file, output_file=None, both=False):
+    def translit_file_cy_lt(self, input_file, output_file=None, both=False):
 
         input_file = open(input_file).read()
         new_file = ""
         rules = self.rule_cy_lt()
+        char = list(ngrams(input_file, 1))
+        i = 0
 
         if output_file is None:
-            for char in input_file:
-                if char in rules.keys():
-                    new_char = rules.get(char)
-                    # print(new_char)
+
+            while i < len(char)-3:
+
+                three = "".join(char[i]+char[i+1]+char[i+2])
+                # print(three)
+                two = "".join(char[i]+char[i+1])
+                # print(two)
+                one = "".join(char[i])
+
+                if three in rules.keys():
+                    new_char = rules.get(three)
+                    # print("3. ok")
                     new_file += "".join(new_char)
                     # print(new_file)
+                    i += 3
+                elif two in rules.keys():
+                    new_char = rules.get(two)
+                    # print("2. ok")
+                    new_file += "".join(new_char)
+                    # print(new_file)
+                    i += 2
+                elif one in rules.keys():
+                    new_char = rules.get(one)
+                    # print("1. ok")
+                    new_file += "".join(new_char)
+                    # print(new_file)
+                    i += 1
                 else:
-                    new_file += "".join(char)
+                    new_file += "".join(one)
+                    # print("4. pas ok")
+                    i += 1
+            # print(i)
+            two = "".join(char[i]+char[i+1])
+            if two in rules.keys():
+                new_file += "".join(rules.get(two))
+            elif char[i] and char[i+1] in rules.keys():
+                new_file += "".join(rules.get(char[i]))
+                new_file += "".join(rules.get(char[i+1]))
+            else:
+                new_file += "".join(two)
 
-            return new_file
         else:
             output_file = open(output_file, "w")
 
-            for char in input_file:
-                if char in rules.keys():
-                    new_char = rules.get(char)
+            while i < len(char) - 3:
+
+                three = "".join(char[i] + char[i + 1] + char[i + 2])
+                # print(three)
+                two = "".join(char[i] + char[i + 1])
+                # print(two)
+                one = "".join(char[i])
+
+                if three in rules.keys():
+                    new_char = rules.get(three)
+                    # print("3. ok")
                     new_file += "".join(new_char)
+                    # print(new_file)
+                    i += 3
+                elif two in rules.keys():
+                    new_char = rules.get(two)
+                    # print("2. ok")
+                    new_file += "".join(new_char)
+                    # print(new_file)
+                    i += 2
+                elif one in rules.keys():
+                    new_char = rules.get(one)
+                    # print("1. ok")
+                    new_file += "".join(new_char)
+                    # print(new_file)
+                    i += 1
                 else:
-                    new_file += "".join(char)
+                    new_file += "".join(one)
+                    # print("4. pas ok")
+                    i += 1
+            # print(i)
+            two = "".join(char[i] + char[i + 1])
+            if two in rules.keys():
+                new_file += "".join(rules.get(two))
+            elif char[i] and char[i + 1] in rules.keys():
+                new_file += "".join(rules.get(char[i]))
+                new_file += "".join(rules.get(char[i + 1]))
+            else:
+                new_file += "".join(two)
 
             if both:
                 output_file.write(f"Texte original :\n{input_file}")
                 output_file.write("\n----------------------\n")
                 output_file.write(f"Texte translittérer :\n{new_file}")
+
             else:
                 output_file.write(f"Texte translittérer :\n{new_file}")
 
 
-
 t = Translitteration()
-t.translit_cy_lt("tweet_test_transli.txt", "output_test_translit.txt", both=True)
+t.translit_file_cy_lt("tweet_test_transli.txt", output_file="output_test_translit.txt", both=True)
